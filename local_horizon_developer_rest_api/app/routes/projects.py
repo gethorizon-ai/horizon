c19b46fba7145cff3c7d762ef41824b1f4680ab8
ltawfik <linatawfik9@gmail.com>
2023-03-26 23:27:35 -0700

    changed evaluation dataset datatype to binary and added methods to upload, download and list evaluation datasets

--- local_horizon_developer_rest_api/app/routes/projects.py
+++ local_horizon_developer_rest_api/app/routes/projects.py
@@ -1,10 +1,18 @@
-from flask import request
 from flask_restful import Resource, reqparse
 from app.models import Project, Task
 from app import db, api
 from app.routes.users import api_key_required
 from sqlalchemy.exc import IntegrityError
 from flask_restful import Api
+from flask import request, send_file, make_response
+from io import BytesIO
+import os
+import csv
+from werkzeug.utils import secure_filename
+from werkzeug.datastructures import FileStorage
+from flask import Response
+
+ALLOWED_EXTENSIONS = {'csv'}
 
 
 class ListProjectsAPI(Resource):
@@ -23,7 +31,7 @@ class CreateProjectAPI(Resource):
         parser.add_argument('user_id', type=int,
                             required=True, help='User ID is required')
         args = parser.parse_args()
-        print(args)
+
         project = Project(name=args['name'], user_id=args['user_id'])
 
         try:
@@ -32,6 +40,12 @@ class CreateProjectAPI(Resource):
         except IntegrityError as e:
             db.session.rollback()
             return {"error": str(e)}, 400
+
+        # Check if a file was uploaded
+        if 'evaluation_datasets' in request.files:
+            file = request.files['evaluation_datasets']
+            project.evaluation_datasets = file.read()
+            db.session.commit()
 
         return {"message": "Project created successfully", "project": project.to_dict()}, 201
 
@@ -81,7 +95,73 @@ class ProjectAPI(Resource):
         return {"message": "Project deleted successfully"}, 200
 
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+class UploadEvaluationDatasetsAPI(Resource):
+    @api_key_required
+    def post(self, project_id):
+        project = Project.query.get(project_id)
+        if not project:
+            return {"error": "Project not found"}, 404
+
+        if 'evaluation_datasets' not in request.files:
+            return {"error": "No file provided"}, 400
+
+        file = request.files['evaluation_datasets']
+
+        if not allowed_file(file.filename):
+            return {"error": "Invalid file type. Only CSV files are allowed."}, 400
+
+        project.evaluation_datasets = file.read()
+        db.session.commit()
+
+        return {"message": "Evaluation datasets uploaded successfully", "project": project.to_dict()}, 200
+
+
+class ViewEvaluationDatasetsAPI(Resource):
+    @api_key_required
+    def get(self, project_id):
+        project = Project.query.get(project_id)
+        if not project:
+            return {"error": "Project not found"}, 404
+
+        if not project.evaluation_datasets:
+            return {"error": "No evaluation_datasets file associated with this project"}, 404
+
+        evaluation_datasets_file = BytesIO(project.evaluation_datasets)
+        reader = csv.DictReader(
+            evaluation_datasets_file.read().decode('utf-8').splitlines())
+
+        evaluation_datasets = [row for row in reader]
+        return {"evaluation_datasets": evaluation_datasets}, 200
+
+
+class EvaluationDatasetsAPI(Resource):
+    def get(self, project_id):
+        project = Project.query.get(project_id)
+        if not project:
+            return {"error": "Project not found"}, 404
+
+        if not project.evaluation_datasets:
+            return {"error": "No evaluation datasets available"}, 404
+
+        response = make_response(send_file(BytesIO(project.evaluation_datasets),
+                                           attachment_filename='evaluation_datasets.csv',
+                                           as_attachment=True))
+        response.headers['Content-Type'] = 'text/csv'
+        return response
+
+
 def register_routes(api):
     api.add_resource(ListProjectsAPI, '/api/projects')
     api.add_resource(CreateProjectAPI, '/api/projects/create')
     api.add_resource(ProjectAPI, '/api/projects/<int:project_id>')
+    api.add_resource(UploadEvaluationDatasetsAPI,
+                     '/api/projects/<int:project_id>/upload_evaluation_datasets')
+    api.add_resource(ViewEvaluationDatasetsAPI,
+                     '/api/projects/<int:project_id>/view_evaluation_datasets')
+    api.add_resource(EvaluationDatasetsAPI,
+                     '/api/projects/<int:project_id>/evaluation_datasets')
