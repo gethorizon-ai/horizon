4ade5d9331960f398bfbda9f2de57de530a2ceea
ltawfik <linatawfik9@gmail.com>
2023-04-25 19:40:24 -0400

    modified after adding iam role to ec2 instance and iam access keys no longer required

--- local_horizon_developer_rest_api/app/models/component/user/user.py
+++ local_horizon_developer_rest_api/app/models/component/user/user.py
@@ -13,48 +13,13 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
-    api_key = db.Column(db.String(36), unique=True,
-                        nullable=False, default=generate_api_key)
-    projects = db.relationship('Project', backref='user', lazy='dynamic')
-
-    def __init__(self, username, email, password=None, hashed_password=None):
-        self.username = username
-        self.email = email
-        if hashed_password:
-            self.password_hash = hashed_password
-        elif password:
-            self.set_password(password)
-        self.api_key = generate_api_key()
-
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password, method='sha256')
-
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
-
-    def to_dict(self):
-        return {
-            'id': self.id,
-            'username': self.username,
-            'email': self.email,
-            'api_key': self.api_key
-        }
-
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
     api_key = db.Column(db.String(36), unique=True,
                         nullable=False, default=generate_api_key)
     projects = db.relationship('Project', backref='user', lazy='dynamic')
 
     # Initialize the Cognito client
     cognito = boto3.client('cognito-idp',
-                           region_name=Config.AWS_REGION,
-                           aws_access_key_id=Config.AWS_ACCESS_KEY,
-                           aws_secret_access_key=Config.AWS_SECRET_KEY)
+                           region_name=Config.AWS_REGION)
 
     def __init__(self, username, email, password=None, cognito_user=None):
         self.username = username
