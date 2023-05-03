ee34951067c31cdf1021eeb3c571d33a20cbe81b
ltawfik <linatawfik9@gmail.com>
2023-05-02 22:24:03 -0400

    changed mysqlclient requirment to not specify version number

--- horizon_api/config.py
+++ horizon_api/config.py
@@ -1,25 +1,13 @@
 import os
 
-# from urllib.parse import quote
-
-# password = "@Mhof98s#fr55"
-# encoded_password = quote(password)
-
-# basedir = os.path.abspath(os.path.dirname(__file__))
-
 
 class Config:
-    # SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-    #     f'mysql+pymysql://ltawfik:{encoded_password}@localhost/horizon_local'
-    # SQLALCHEMY_TRACK_MODIFICATIONS = False
-    # CORS_HEADERS = 'Content-Type'
 
     username = os.environ.get("DB_USERNAME")
     password = os.environ.get("DB_PASSWORD")
     hostname = os.environ.get("DB_HOSTNAME")
     port = os.environ.get("DB_PORT")
-    database_name = "horizon_aditya"  # os.environ.get("DB_NAME")
+    database_name = os.environ.get("DB_NAME")
 
     SQLALCHEMY_DATABASE_URI = f"mysql+mysqlconnector://{username}:{password}@{hostname}:{port}/{database_name}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
