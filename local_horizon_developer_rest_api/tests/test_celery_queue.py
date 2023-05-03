e11ad682c9a10154cdf1c6db788c6d8cc5893aea
Aditya Bhagavathi <adityabhagavathi@Adityas-MacBook-Air.local>
2023-05-03 10:29:49 -0700

    Changed package name

--- local_horizon_developer_rest_api/tests/test_celery_queue.py
+++ local_horizon_developer_rest_api/tests/test_celery_queue.py
@@ -1,6 +1,6 @@
 """Stress tests Celery queue on server by rapidly issuing task generation requests."""
 
-import horizonai
+import horizon_ai
 from config import Config
 import time
 
@@ -18,34 +18,34 @@ def test_celery_queue():
     # Generate Horizon API key
     test_email = Config.HORIZON_TEST_EMAIL
     test_password = Config.HORIZON_TEST_PASSWORD
-    generate_new_api_key_response = horizonai.generate_new_api_key(
+    generate_new_api_key_response = horizon_ai.generate_new_api_key(
         test_email, test_password
     )
     test_api_key = generate_new_api_key_response["api_key"]
 
-    horizonai.api_key = test_api_key
-    horizonai.anthropic_api_key = Config.HORIZON_ANTHROPIC_API_KEY
+    horizon_ai.api_key = test_api_key
+    horizon_ai.anthropic_api_key = Config.HORIZON_ANTHROPIC_API_KEY
 
     # Create new project
-    create_project_response = horizonai.create_project(project_name)
+    create_project_response = horizon_ai.create_project(project_name)
     project_id = create_project_response["project"]["id"]
 
     # Conduct stress test
     num_requests = 50
     for i in range(num_requests):
         # Create task record
-        task_creation_response = horizonai.create_task(
+        task_creation_response = horizon_ai.create_task(
             task_name, task_type, project_id, allowed_models
         )
         task_id = task_creation_response["task"]["id"]
 
         # Upload evaluation dataset
-        upload_dataset_response = horizonai.upload_evaluation_dataset(
+        upload_dataset_response = horizon_ai.upload_evaluation_dataset(
             task_id, evaluation_data_file_path
         )
 
         # Generate task
-        generate_response = horizonai.generate_task(task_id, objective)
+        generate_response = horizon_ai.generate_task(task_id, objective)
 
         # Pause briefly
         time.sleep(1)
