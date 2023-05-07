48b0e04d024bf44e1b36c03e125a9834e735814d
Aditya Bhagavathi <adityabhagavathi@Adityas-MacBook-Air.local>
2023-05-07 09:57:51 -0700

    Added print debug statements

--- horizon_api/app/utilities/run/prompt_generation_algorithm_parameters.py
+++ horizon_api/app/utilities/run/prompt_generation_algorithm_parameters.py
@@ -3,12 +3,12 @@
 # TODO: update to parameters for full prompt generation algorithm
 PROMPT_GENERATION_ALGORITHM_PARAMETERS = {
     "stage_1": {
-        "num_prompts_user_objective": 3,  # 10,
-        "num_prompts_pattern_role_play": 3,  # 10,
-        "num_prompts_user_objective_training_data": 3,  # 10,
+        "num_prompts_user_objective": 1,  # 10,
+        "num_prompts_pattern_role_play": 1,  # 10,
+        "num_prompts_user_objective_training_data": 1,  # 10,
         "num_variants": 1,  # 2,
-        "num_clusters": 10,  # 20,
-        "num_shortlist": 3,  # 10,
+        "num_clusters": 4,  # 20,
+        "num_shortlist": 1,  # 10,
         "num_iterations": 3,  # 3,
     },
     "stage_2": {"num_shortlist": 1},
