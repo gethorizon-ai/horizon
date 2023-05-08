a6166b2cdeb23e401851e89ce297cbff08d3bfa7
Aditya Bhagavathi <adityabhagavathi@Adityas-MacBook-Air.local>
2023-05-08 14:25:57 -0700

    Bug fix

--- horizon_api/app/utilities/output_schema/output_schema.py
+++ horizon_api/app/utilities/output_schema/output_schema.py
@@ -4,6 +4,7 @@ from app.utilities.S3.s3_util import download_file_from_s3_and_save_locally
 from pydantic import BaseModel
 import os
 import importlib
+import sys
 
 
 def get_pydantic_object_from_s3(pydantic_model_s3_key: str) -> BaseModel:
@@ -21,18 +22,17 @@ def get_pydantic_object_from_s3(pydantic_model_s3_key: str) -> BaseModel:
     )
 
     # Try to import Pydantic model
-    pydantic_model_file_path = download_file_from_s3_and_save_locally(
-        pydantic_model_s3_key
-    )
     pydantic_module_name = os.path.basename(pydantic_model_file_path)[:-3]
     pydantic_module_spec = importlib.util.spec_from_file_location(
         pydantic_module_name, pydantic_model_file_path
     )
     pydantic_module_object = importlib.util.module_from_spec(pydantic_module_spec)
+    sys.modules[pydantic_module_name] = pydantic_module_object
     pydantic_module_spec.loader.exec_module(pydantic_module_object)
 
     # Pydantic class / object assumed to be called "OutputSchema"
     pydantic_object = pydantic_module_object.OutputSchema
+    del sys.modules[pydantic_module_name]
 
     # Remove temp file
     os.remove(pydantic_model_file_path)
