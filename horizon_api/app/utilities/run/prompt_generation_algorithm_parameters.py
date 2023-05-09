d6bf73f800c925a33fa96f8d24e1b9566efeacc0
Aditya Bhagavathi <adityabhagavathi@Adityas-MacBook-Air.local>
2023-05-09 10:46:00 -0700

    Added test data and reduced algo scope temporarily

--- horizon_api/app/utilities/run/prompt_generation_algorithm_parameters.py
+++ horizon_api/app/utilities/run/prompt_generation_algorithm_parameters.py
@@ -1,18 +1,19 @@
 """Defines parameters used for prompt generation algorithm."""
 
+# TODO: update to parameters for full prompt generation algorithm
 PROMPT_GENERATION_ALGORITHM_PARAMETERS = {
     "stage_1": {
-        "num_prompts_user_objective": 10,
-        "num_prompts_pattern_role_play": 10,
-        "num_prompts_user_objective_training_data": 10,
-        "num_variants": 2,
-        "num_clusters": 20,
-        "num_shortlist": 10,
-        "num_iterations": 3,
+        "num_prompts_user_objective": 1,  # 10,
+        "num_prompts_pattern_role_play": 1,  # 10,
+        "num_prompts_user_objective_training_data": 1,  # 10,
+        "num_variants": 1,  # 2,
+        "num_clusters": 4,  # 20,
+        "num_shortlist": 1,  # 10,
+        "num_iterations": 3,  # 3,
     },
     "stage_2": {"num_shortlist": 1},
     "stage_3": {
-        "num_prompts_temperature_variation": 5,
+        "num_prompts_temperature_variation": 2,  # 5,
         "num_shortlist": 1,
         "num_iterations": 1,  # 3,
     },
