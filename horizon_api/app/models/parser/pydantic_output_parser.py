dc8d9314fe383424b72de1ec9dcc895c5f5efaf1
Aditya Bhagavathi <adityabhagavathi@Adityas-MacBook-Air.local>
2023-05-09 14:34:03 -0700

    Reimplemented parse function to handle invalid json control characters

--- horizon_api/app/models/parser/pydantic_output_parser.py
+++ horizon_api/app/models/parser/pydantic_output_parser.py
@@ -4,6 +4,14 @@ from .base import BaseParser
 from langchain.output_parsers import (
     PydanticOutputParser as PydanticOutputParserOriginal,
 )
+from langchain.schema import OutputParserException
+import json
+import re
+from typing import TypeVar
+
+from pydantic import BaseModel, ValidationError
+
+T = TypeVar("T", bound=BaseModel)
 
 
 class PydanticOutputParser(BaseParser, PydanticOutputParserOriginal):
@@ -23,3 +31,32 @@ class PydanticOutputParser(BaseParser, PydanticOutputParserOriginal):
             .replace("{", "{{")
             .replace("}", "}}")
         )
+
+    def parse(self, text: str) -> T:
+        """Re-implemented LangChain parse function to change json.loads to strict=False so that control characters (e.g., newlines)
+        are accepted.
+
+        Args:
+            text (str): output from llm to parse.
+
+        Raises:
+            OutputParserException: error when output cannot parsed according to output schema.
+
+        Returns:
+            T: Pydantic model object.
+        """
+        try:
+            # Greedy search for 1st json candidate.
+            match = re.search(
+                r"\{.*\}", text.strip(), re.MULTILINE | re.IGNORECASE | re.DOTALL
+            )
+            json_str = ""
+            if match:
+                json_str = match.group()
+            json_object = json.loads(json_str, strict=False)  # Edited to strict=False
+            return self.pydantic_object.parse_obj(json_object)
+
+        except (json.JSONDecodeError, ValidationError) as e:
+            name = self.pydantic_object.__name__
+            msg = f"Failed to parse {name} from completion {text}. Got: {e}"
+            raise OutputParserException(msg)
