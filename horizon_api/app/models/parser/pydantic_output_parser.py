b6dbb139ea8574b3c1bb34fdb8d19e56e00c6cf2
Aditya Bhagavathi <adityabhagavathi@Adityas-MacBook-Air.local>
2023-05-09 21:21:50 -0700

    Debugging and reverting to original prompt algo parameters

--- horizon_api/app/models/parser/pydantic_output_parser.py
+++ horizon_api/app/models/parser/pydantic_output_parser.py
@@ -8,10 +8,18 @@ from langchain.schema import OutputParserException
 import json
 import re
 from typing import TypeVar
-
 from pydantic import BaseModel, ValidationError
 
 T = TypeVar("T", bound=BaseModel)
+
+PYDANTIC_FORMAT_INSTRUCTIONS = """The output should be formatted as a JSON instance that conforms to the JSON schema below.
+
+As an example, for the schema {"properties": {"foo": {"title": "Foo", "description": "a list of strings", "type": "array", "items": {"type": "string"}}}, "required": ["foo"]}}, the object {"foo": ["bar", "baz"]} is a well-formatted instance of the schema. The object {"properties": {"foo": ["bar", "baz"]}} is not well-formatted.
+
+Here is the output schema:
+```
+{schema}
+```"""
 
 
 class PydanticOutputParser(BaseParser, PydanticOutputParserOriginal):
@@ -20,17 +28,25 @@ class PydanticOutputParser(BaseParser, PydanticOutputParserOriginal):
     def get_format_instructions(self) -> str:
         """Returns output format instructions to append to prompt prefix.
 
-        Currently, adds two newlines to standard response from LangChain original response.
+        Currently, adds two newlines and removes an unnecessary newline from LangChain's original response.
 
         Returns:
             str: output format instructions to append to prompt prefix.
         """
-        return (
-            "\n\n==\nOUTPUT FORMAT:\n\n"
-            + PydanticOutputParserOriginal.get_format_instructions(self)
-            .replace("{", "{{")
-            .replace("}", "}}")
-        )
+        schema = self.pydantic_object.schema()
+
+        # Remove extraneous fields.
+        reduced_schema = schema
+        if "title" in reduced_schema:
+            del reduced_schema["title"]
+        if "type" in reduced_schema:
+            del reduced_schema["type"]
+        # Ensure json in context is well-formed with double quotes.
+        schema_str = json.dumps(reduced_schema)
+
+        return "\n\n==\nOUTPUT FORMAT:\n\n" + PYDANTIC_FORMAT_INSTRUCTIONS.format(
+            schema=schema_str
+        ).replace("{", "{{").replace("}", "}}")
 
     def parse(self, text: str) -> T:
         """Attempts to parse given text according to Pydantic model stored inside.
