358e757e0fb5f595634df85399da613fcbef216e
Aditya Bhagavathi <adityabhagavathi@Adityas-MacBook-Air.local>
2023-05-09 21:41:45 -0700

    Debugging

--- horizon_api/app/utilities/run/prompt_generation_algorithm_parameters.py
+++ horizon_api/app/utilities/run/prompt_generation_algorithm_parameters.py
@@ -7,7 +7,7 @@ PROMPT_GENERATION_ALGORITHM_PARAMETERS = {
         "num_prompts_pattern_role_play": 10,  # 1,
         "num_prompts_user_objective_training_data": 10,  # 1,
         "num_variants": 2,  # 1,
-        "num_clusters": 2,  # 5,
+        "num_clusters": 20,  # 5,
         "num_shortlist": 10,  # 2,
         "num_iterations": 3,  # 3,
     },
