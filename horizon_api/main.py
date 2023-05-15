ab1b6be0bcec7a071f888e443815ff66f47f6778
Aditya Bhagavathi <adityabhagavathi@Adityas-MacBook-Air.local>
2023-05-15 15:25:20 -0500

    Removed dev server setup

--- horizon_api/main.py
+++ horizon_api/main.py
@@ -6,17 +6,16 @@ import ssl
 app = create_app()
 
 if __name__ == "__main__":
-    # TODO: uncomment
-    # context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-    # context.load_cert_chain('/home/ec2-user/ssl/cert.pem',
-    #                         '/home/ec2-user/ssl/priv_key.pem')
-    # app.run(host='0.0.0.0', port=5000, ssl_context=context)
-    app.run(host="0.0.0.0", port=5000)
+    context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+    context.load_cert_chain(
+        "/home/ec2-user/ssl/cert.pem", "/home/ec2-user/ssl/priv_key.pem"
+    )
+    app.run(host="0.0.0.0", port=5000, ssl_context=context)
 
 
-# limiter = Limiter(
-#     app,
-#     # Pass 'key_func' as a keyword argument
-#     key_func=lambda: request.headers.get('X-Api-Key'),
-#     default_limits=["500 per day", "50 per hour"]  # Set default rate limits
-# )
+limiter = Limiter(
+    app,
+    # Pass 'key_func' as a keyword argument
+    key_func=lambda: request.headers.get("X-Api-Key"),
+    default_limits=["500 per day", "50 per hour"],  # Set default rate limits
+)
