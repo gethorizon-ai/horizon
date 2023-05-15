6a4197d9b69c902890bb92e1385eb2b7c5d34c5d
Aditya Bhagavathi <adityabhagavathi@Adityas-MacBook-Air.local>
2023-05-15 15:27:00 -0500

    Clean up

--- horizon_api/app/models/embedding/open_ai.py
+++ horizon_api/app/models/embedding/open_ai.py
@@ -304,37 +304,3 @@ class OpenAIEmbeddings(BaseModel, Embeddings):
 
         embedding = self._embedding_func(text, engine=self.deployment)
         return embedding
-
-
-# from .base import BaseEmbeddings
-# from langchain.embeddings import OpenAIEmbeddings as OpenAIEmbeddingsOriginal
-# import openai
-# from pydantic import root_validator
-# from typing import (
-#     Any,
-#     Dict,
-#     Literal,
-#     Set,
-#     Union,
-# )
-
-
-# class OpenAIEmbeddings(OpenAIEmbeddingsOriginal):
-#     @root_validator()
-#     def validate_environment(cls, values: Dict) -> Dict:
-#         values = super().validate_environment(values)
-#         return values
-
-#     def __init__(self, *args, **kwargs):
-#         super().__init__(*args, **kwargs)
-
-#     # def embed_documents(self, *args: Any, **kwargs: Any) -> Any:
-#     #     # Reset OpenAI API key in case it was changed by other processes (e.g., llm inference calls)
-#     #     openai.api_key = self.openai_api_key
-#     #     return super(OpenAIEmbeddingsOriginal, self).embed_documents(*args, **kwargs)
-
-#     # def embed_query(self, *args: Any, **kwargs: Any) -> Any:
-#     #     # Reset OpenAI API key in case it was changed by other processes (e.g., llm inference calls)
-#     #     openai.api_key = self.openai_api_key
-#     #     return super(OpenAIEmbeddingsOriginal, self).embed_query(*args, **kwargs)
-#     pass
