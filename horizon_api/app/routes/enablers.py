4664326495093ec0a0653485bcaf553c80c9dc52
Aditya Bhagavathi <adityabhagavathi@Adityas-MacBook-Air.local>
2023-05-22 17:26:57 -0700

    Debugging

--- horizon_api/app/routes/enablers.py
+++ horizon_api/app/routes/enablers.py
@@ -41,13 +41,13 @@ class GenerateSyntheticDataAPI(Resource):
         logging.info("GenerateSyntheticDataAPI: Start processing the request")
 
         parser = reqparse.RequestParser()
-        parser.add_argument(
-            "objective",
-            type=str,
-            required=True,
-            location="files",
-            help="Objective, number of synthetic data, and OpenAI API key are required",
-        )
+        # parser.add_argument(
+        #     "objective",
+        #     type=str,
+        #     required=True,
+        #     location="files",
+        #     help="Objective, number of synthetic data, and OpenAI API key are required",
+        # )
         # parser.add_argument(
         #     "json_data",
         #     type=str,
