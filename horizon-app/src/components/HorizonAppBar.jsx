c3e52607cc4b46bc8ba0df1e6531f2860d0fdd52
ltawfik <linatawfik9@gmail.com>
2023-06-04 22:22:12 -0700

    updated app bar and welcome page ui and added links

--- horizon-app/src/components/HorizonAppBar.jsx
+++ horizon-app/src/components/HorizonAppBar.jsx
@@ -1,206 +1,93 @@
 import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
-import AccountCircle from '@mui/icons-material/AccountCircle';
+import IconButton from '@mui/material/IconButton';
 import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
 import { useLocation, useNavigate } from 'react-router-dom';
+import TwitterIcon from '@mui/icons-material/Twitter';
 
 function HorizonAppBar({ user = undefined, signOut = undefined }) {
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
-
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
-
   const navigate = useNavigate();
   const location = useLocation().pathname;
 
   const goToLoginPage = () => {
-    if (location === "/login") {
-      handleCloseUserMenu();
-    } else {
+    if (location !== "/login") {
       navigate("/login");
     }
   };
-
-  // const goToApiKeyPage = () => {
-  //   if (location === "/account/api_key") {
-  //     handleCloseUserMenu();
-  //   } else {
-  //     navigate("/account/api_key");
-  //   }
-  // };
 
   const goToDocumentationPage = () => {
     window.open("http://docs.gethorizon.ai", "_blank")
   };
 
-  const pagesAndHandlers = {
-    'Documentation': goToDocumentationPage
-  };
-
-  const settingsAndHandlers = {
-    // 'View API Key': goToApiKeyPage,
-    'Logout': signOut
-  };
-
-  const showAccountOptions = () => {
-    if (user) {
-      return (
-        <div>
-          <Tooltip title="Open settings">
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenUserMenu}
-              sx={{ p: 0 }}
-              color="inherit"
-            >
-              <AccountCircle />
-            </IconButton>
-          </Tooltip>
-          <Menu
-            sx={{ mt: '45px' }}
-            id="menu-appbar"
-            anchorEl={anchorElUser}
-            anchorOrigin={{
-              vertical: 'top',
-              horizontal: 'right',
-            }}
-            keepMounted
-            transformOrigin={{
-              vertical: 'top',
-              horizontal: 'right',
-            }}
-            open={Boolean(anchorElUser)}
-            onClose={handleCloseUserMenu}
-          >
-            {Object.keys(settingsAndHandlers).map((setting) => (
-              <MenuItem key={setting} onClick={settingsAndHandlers[setting]}>
-                <Typography textAlign="center">{setting}</Typography>
-              </MenuItem>
-            ))}
-          </Menu>
-        </div>
-      )
-    } else {
-      return (
-        <Button variant='outlined' sx={{ my: 2, display: 'block', color: 'black' }} onClick={goToLoginPage}>Login</Button>
-      )
+  const goToOverviewPage = () => {
+    if (location !== "/") {
+      navigate("/");
     }
   };
 
-  return (
+  const goToExamplesPage = () => {
+    window.open("https://docs.gethorizon.ai/reference/example-use-cases", "_blank");
+  };
 
-    <AppBar position="relative" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
-      <Container maxWidth="False">
-        <Toolbar disableGutters>
+  const pagesAndHandlers = {
+    'Overview': goToOverviewPage,
+    'Documentation': goToDocumentationPage,
+    'Examples': goToExamplesPage
+  };
+
+  const settingsAndHandlers = {
+    'Logout': signOut
+  };
+
+  return (
+    <AppBar position="relative" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1, backgroundColor: '#F9FAFB' }}>
+      <Container maxWidth="100%" disableGutters sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', py: 2, px:2 }}>
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
           <Typography
             variant="h5"
-            noWrap
             component="a"
             href="/"
             sx={{
-              mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              flexGrow: 1,
-              fontWeight: 'bold',
               color: '#1d44b8',
+              fontFamily: 'Open Sans, sans-serif',
+              fontSize: '1.5rem',
+              fontWeight: 1000,
+              lineHeight: 1,
+              display: 'flex',
+              alignItems: 'center',
               textDecoration: 'none',
             }}
           >
             Horizon AI
           </Typography>
-
-          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "right"
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right"
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: "block", md: "none" }
-              }}
-            >
-              {Object.keys(pagesAndHandlers).map((page) => (
-                <MenuItem key={page} onClick={pagesAndHandlers[page]}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-
-          <Box sx={{ flexGrow: 1 }} >
-            <Typography
-              variant="h5"
-              noWrap
-              component="a"
-              href=""
-              sx={{
-                mr: 2,
-                display: { xs: 'flex', md: 'none' },
-                flexGrow: 1,
-                fontWeight: 700,
-                color: 'inherit',
-                textDecoration: 'none',
-              }}
-            >
-              Horizon AI
-            </Typography>
-          </Box>
-
-          <Box sx={{ flexGrow: 0, display: { xs: 'none', md: 'flex' } }}>
-            {Object.keys(pagesAndHandlers).map((page) => (
-              <Button key={page} onClick={pagesAndHandlers[page]} sx={{ my: 2, display: 'block', color: 'black' }}>
-                {page}
-              </Button>
-            ))}
-          </Box>
-          <Box sx={{ flexGrow: 0, pl: 2 }}>
-            {showAccountOptions()}
-          </Box>
-        </Toolbar>
+          {Object.keys(pagesAndHandlers).map((page) => (
+            <Button key={page} onClick={pagesAndHandlers[page]} sx={{ color: '#4B5563', ml: 3, textTransform: "none", fontSize: "1rem", "&:hover": { backgroundColor: "#E5E7EB" } }}>
+              {page}
+            </Button>
+          ))}
+        </Box>
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          <IconButton color="inherit" sx={{ color: '#1F2937' }} onClick={() => window.open('https://twitter.com', '_blank')}>
+            <TwitterIcon />
+          </IconButton>
+          <Button
+            onClick={settingsAndHandlers['Logout']}
+            sx={{
+              color: 'rgba(31, 41, 55, 0.5)',
+              ml: 2,
+              textTransform: 'none',
+              fontSize: '1rem',
+              '&:hover': {
+                backgroundColor: '#E5E7EB',
+              },
+            }}
+          >
+            Logout
+          </Button>
+        </Box>
       </Container>
     </AppBar>
   );
--- horizon-app/src/pages/account/WelcomePage.jsx
+++ horizon-app/src/pages/account/WelcomePage.jsx
@@ -1,96 +1,181 @@
-import { Box, Container, Typography } from "@mui/material";
+import { Box, Container, Typography, Link, List, ListItem } from "@mui/material";
 import HorizonAppBar from "../../components/HorizonAppBar";
-import AccountDrawer from "../../components/AccountDrawer";
-import { DefaultCopyField } from '@eisberg-labs/mui-copy-field';
 import { useEffect, useState } from "react";
 import { Auth } from "aws-amplify";
 import { Navigate } from "react-router-dom";
-import { Code, ThemeProvider, createTheme } from '@mui/material';
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { vscDarkPlus } from 'react-syntax-highlighter/dist/esm/styles/prism';
-
-const drawerWidth = 240;
 
 const initialState = {
-    user: undefined,
-    isLoggedIn: false,
-    loading: true
+  user: undefined,
+  isLoggedIn: false,
+  loading: true,
 };
 
+const items = [
+  {
+    title: "Quickstart Tutorial",
+    href: "https://docs.gethorizon.ai/quickstart",
+    description: "Learn how to use Horizon by building an LLM function.",
+  },
+  {
+    title: "Installation",
+    href: "https://docs.gethorizon.ai/reference/command-line-interface",
+    description: "Learn how to install Horizon and use it in production.",
+  },
+  {
+    title: "Examples",
+    href: "https://docs.gethorizon.ai/reference/example-use-cases",
+    description: "Explore common tasks and use cases solved by Horizon.",
+  },
+];
+
 function WelcomePage() {
-    const [state, setState] = useState(initialState);
-    useEffect(() => {
-        async function checkUser() {
-            await Auth.currentAuthenticatedUser({
-                bypassCache: false
-            }).then(user => {
-                setState({
-                    ...initialState,
-                    user: user,
-                    isLoggedIn: true,
-                    loading: false
-                });
-                console.log(user)
-            }).catch(err => {
-                console.log(err);
-                setState({
-                    ...initialState,
-                    user: undefined,
-                    isLoggedIn: false,
-                    loading: false
-                });
-            });
-        }
-        checkUser();
-    }, []);
+  const [state, setState] = useState(initialState);
+  useEffect(() => {
+    async function checkUser() {
+      try {
+        const user = await Auth.currentAuthenticatedUser({
+          bypassCache: false,
+        });
+        setState({
+          ...initialState,
+          user: user,
+          isLoggedIn: true,
+          loading: false,
+        });
+        console.log(user);
+      } catch (err) {
+        console.log(err);
+        setState({
+          ...initialState,
+          user: undefined,
+          isLoggedIn: false,
+          loading: false,
+        });
+      }
+    }
+    checkUser();
+  }, []);
 
-    const signOut = async () => {
-        try {
-            await Auth.signOut({ global: true });
-            setState({ ...initialState, loading: false });
-        } catch (error) {
-            console.log('error signing out: ', error);
-        }
+  const signOut = async () => {
+    try {
+      await Auth.signOut({ global: true });
+      setState({ ...initialState, loading: false });
+    } catch (error) {
+      console.log('error signing out: ', error);
     }
+  };
 
-    if (state.loading) {
-        return (<div className="main-box">
-            <HorizonAppBar />
-            <p>Loading...</p>
-        </div>)
-    }
-    if (!state.isLoggedIn) {
-        return (
-            <Navigate to="/login" />
-        )
-    }
+  if (state.loading) {
     return (
-        <Box>
-            <HorizonAppBar user={state.user} signOut={signOut} />
-            <Box component="drawer" sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}>
-                <AccountDrawer />
-            </Box>
-            <Box component="main" sx={{ flexGrow: 1, mt: 3, ml: { sm: `${drawerWidth}px` } }}>
-                <Container maxWidth="xl">
-                    <Typography variant="h4" align="left" sx={{ mb: 3, fontWeight: 700 }} >
-                        Welcome!
-                    </Typography>
-                    <Typography variant="body1">
-                        First step - go to your command line to get your API key.<br /><br />
-
-                        Detailed guidance on how to use Horizon is provided <a href="https://docs.gethorizon.ai/quickstart#2-install-the-library-and-generate-your-horizon-api-key">here</a>.<br /><br />
-                    </Typography>
-                    <SyntaxHighlighter language="bash">
-                        {`# Install Horizon library via pip
-pip install horizonai
-
-# Get your API key
-horizonai user api-key`}
-                    </SyntaxHighlighter>
-                </Container>
-            </Box>
-        </Box >
+      <div className="main-box">
+        <HorizonAppBar />
+        <p>Loading...</p>
+      </div>
     );
+  }
+
+  if (!state.isLoggedIn) {
+    return <Navigate to="/login" />;
+  }
+
+  return (
+    <Box sx={{ display: 'flex', minHeight: '100vh', flexDirection: 'column', mb: 5 }}>
+      <HorizonAppBar user={state.user} signOut={signOut} />
+      <Box component="main" sx={{ flexGrow: 1, mt: 3, mb: 5 }}>
+        <Container maxWidth="xl">
+          <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', mb: 5 }}>
+            <Box sx={{ maxWidth: '60%', textAlign: 'center', px: { xs: 2, sm: 3, md: 10, mb: 5 } }}>
+              <Typography variant="h4" sx={{ fontWeight: 700 }}>
+                Welcome to Horizon AI
+              </Typography>
+              <Box sx={{ height: '20px' }} />
+              <Typography variant="body1">
+                Horizon provides production-grade LLM deployments with its high-level abstraction to simplify your deployment process while improving quality, cost, and latency.
+              </Typography>
+            </Box>
+          </Box>
+          <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+            <Box
+              sx={{
+                background: 'linear-gradient(180deg, #DB4E66 0%, #A24688 39.57%, #4E3ABA 100%)',
+                p: 4,
+                borderRadius: '8px',
+                minHeight: 300,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+                marginRight: 4,
+                '&:hover': {
+                  backgroundColor: '#E5E7EB',
+                  cursor: 'pointer',
+                },
+                '&:active': {
+                  backgroundColor: '#CBD5E0',
+                },
+              }}
+            >
+              <Typography variant="h6" sx={{ color: '#fff' }}>
+                Start with Horizon
+              </Typography>
+              <Typography variant="body2" sx={{ color: '#fff' }}>
+                Quickly ramp up to use Horizon
+              </Typography>
+            </Box>
+            <List sx={{
+              color: '#1d44b8',
+              fontFamily: 'Open Sans, sans-serif',
+              fontSize: '1.5rem',
+              fontWeight: 1000,
+              lineHeight: 1,
+              alignItems: 'center',
+              textDecoration: 'none',
+            }}>
+              {items.map((item, index) => (
+                <Box
+                  key={index}
+                  component="div"
+                  sx={{
+                    '&:hover': {
+                      backgroundColor: '#E5E7EB',
+                      cursor: 'pointer',
+                    },
+                    '&:active': {
+                      backgroundColor: '#CBD5E0',
+                    },
+                  }}
+                >
+                  <ListItem>
+                    <Link href={item.href} target="_blank" underline="none" sx={{ color: 'rgba(0, 0, 0, 0.87)' }}>
+                      <Box
+                        sx={{
+                          display: 'flex',
+                          flexDirection: 'column',
+                          alignItems: 'flex-start',
+                          padding: '16px',
+                        }}
+                      >
+                        <Typography variant="h6" sx={{ fontWeight: 'bold' }}>
+                          {item.title}
+                        </Typography>
+                        <Typography variant="body2" sx={{ color: 'rgba(0, 0, 0, 0.54)' }}>
+                          {item.description}
+                        </Typography>
+                      </Box>
+                    </Link>
+                  </ListItem>
+                </Box>
+              ))}
+            </List>
+          </Box>
+        </Container>
+      </Box>
+      <Box component="footer" sx={{ backgroundColor: 'rgba(236, 239, 241, 0.15)', color: 'rgba(15,23,42,0.75)', borderRadius: 1, py: 2, px: 2, mt: 'auto', textAlign: 'center' }}>
+        <Typography variant="body1">Need help? Contact Us:</Typography>
+        <Link href="mailto:team@gethorizon.ai" underline="hover" sx={{ color: 'rgba(15,23,42,0.75)' }}>team@gethorizon.ai</Link>
+      </Box>
+    </Box>
+  );
 }
 
-export default WelcomePage
+export default WelcomePage;
