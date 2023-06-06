f3f6afe9e0791696acacfb8053d543ec898b9cc5
ltawfik <linatawfik9@gmail.com>
2023-06-06 14:35:14 -0700

    Update data_check.py
    increase eval data set size limit to 50 mb from 1 mb

--- horizon_api/app/utilities/dataset_processing/data_check.py
+++ horizon_api/app/utilities/dataset_processing/data_check.py
@@ -91,7 +91,7 @@ def check_evaluation_dataset(
         AssertionError: duplicate rows of data.
     """
     # Check that evaluation data is at most 1 MB file size
-    if os.path.getsize(dataset_file_path) > 1000000:
+    if os.path.getsize(dataset_file_path) > 50000000:
         raise AssertionError("Evaluation dataset can be at most 1 MB large.")
 
     # Try to import evaluation dataset
